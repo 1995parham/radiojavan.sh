4f124a4a01d5644e21d861e21a7b5079e429726b
Parham Alvani <parham.alvani@gmail.com>
2018-07-29 01:42:21 +0430

    Handles status code and query part.

--- radiojavan.sh
+++ radiojavan.sh
@@ -14,7 +14,13 @@ usage() {
 
 
 rj-download() {
-        curl -# -o "$name.mp3" "https://host2.rjmusicmedia.com/media/mp3/mp3-256/$1.mp3"
+        status=$(curl -# -w "%{http_code}" -o "$name.mp3" "https://host2.rjmusicmedia.com/media/mp3/mp3-256/$1.mp3")
+
+        if [ $status -ne 200 ]; then
+                echo $status
+                cat "$name.mp3"
+                rm "$name.mp3"
+        fi
 }
 
 main() {
@@ -29,6 +35,7 @@ main() {
         fi
 
         name=$(basename $1)
+        name=${name%%\?*}
         echo "Download $name from Radiojavan"
 
         rj-download $name
